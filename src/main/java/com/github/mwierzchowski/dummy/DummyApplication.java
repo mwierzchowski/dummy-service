ffeb6962bfeabc6f570c173174be83f5a9042d4e
Marcin Wierzchowski <mwierzch@gmail.com>
2021-01-24 13:30:39 +0100

    Add initialization on startup

--- src/main/java/com/github/mwierzchowski/dummy/DummyApplication.java
+++ src/main/java/com/github/mwierzchowski/dummy/DummyApplication.java
@@ -1,8 +1,13 @@
 package com.github.mwierzchowski.dummy;
 
+import com.github.mwierzchowski.dummy.core.DummyPublisher;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.time.Clock;
 
+@Slf4j
 @EnableAsync
 @EnableCaching
 @EnableScheduling
@@ -21,6 +27,15 @@ public class DummyApplication {
 	@Bean
 	Clock systemClock() {
 		return Clock.systemDefaultZone();
+	}
+
+	@Bean
+	@ConditionalOnProperty(name = "dummy.init-on-startup")
+	ApplicationListener<ApplicationReadyEvent> initializer() {
+		return event -> {
+			LOG.info("Initializing on startup");
+			event.getApplicationContext().getBean(DummyPublisher.class).publishSunset();
+		};
 	}
 
 	public static void main(String[] args) {
